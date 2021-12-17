2c6b692a17319e2753092eb3205bacf81a504524
I20050035 <70992098+I20050035@users.noreply.github.com>
2021-12-17 00:42:26 -0600

    Add files via upload

---
+++ OrdenamientoShell/OrdenamientoShell/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdenamientoShell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pv = new Program();
+            pv.Cargar();
+            pv.Shell();
+            pv.Imprimir();
+        }
+        private int[] vector;
+
+        Random aleatorio = new Random();
+        public void Cargar()
+        {
+            Console.WriteLine("Metodo de Shell Sort");
+            Console.Write("Cuantos longitud del vector:");
+            string linea;
+            linea = Console.ReadLine();
+            int cant;
+            cant = int.Parse(linea);
+            vector = new int[cant];
+            for (int f = 0; f < vector.Length; f++)
+            {
+                int random = aleatorio.Next(0, 100);
+                vector[f] = random;
+            }
+        }
+
+        public void Shell()
+        {
+            int salto = 0;
+            int sw = 0;
+            int auxi = 0;
+            int e = 0;
+            salto = vector.Length / 2;
+            while (salto > 0)
+            {
+                sw = 1;
+                while (sw != 0)
+                {
+                    sw = 0;
+                    e = 1;
+                    while (e <= (vector.Length - salto))
+                    {
+                        if (vector[e - 1] > vector[(e - 1) + salto])
+                        {
+                            auxi = vector[(e - 1) + salto];
+                            vector[(e - 1) + salto] = vector[e - 1];
+                            vector[(e - 1)] = auxi;
+                            sw = 1;
+                        }
+                        e++;
+                    }
+                }
+                salto = salto / 2;
+            }
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("Vector ordenados en forma ascendente");
+            for (int f = 0; f < vector.Length; f++)
+            {
+                Console.Write(vector[f] + "  ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
